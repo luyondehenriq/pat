7c02d5662d662cd39b5cf9f7056ad8cdca68c95a
luyondehenriq <luyondehenriq@gmail.com>
2025-11-08 20:36:52 +0300

    Embed TradingView widget in java.js

--- java.js
+++ java.js
@@ -1,1 +1,21 @@
+// Embed TradingView widget
+new TradingView.widget({
+  "width": "100%",
+  "height": 500,
+  "symbol": "FX:EURUSD",
+  "interval": "D",
+  "timezone": "Etc/UTC",
+  "theme": "dark",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#222",
+  "enable_publishing": false,
+  "allow_symbol_change": true,
+  "studies": [
+    "Moving Average@tv-basicstudies",
+    "MACD@tv-basicstudies"
+  ],
+  "container_id": "tradingview_chart"
+});
 
+// Optionally more JS logic can go here (e.g., smooth scroll, mobile nav toggle)
